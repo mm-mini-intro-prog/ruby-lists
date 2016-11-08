a7916323263dcb6f3243d5f024eb6f0b8f0e49aa
mbMosman <mbmosman@users.noreply.github.com>
2016-11-08 01:41:58 -0600

    new lab

---
+++ README.md
@@ -0,0 +1,12 @@
+# ruby-lists
+Practice working with lists and iterators in Ruby.  
+
+This lab contains 5 exercises.  Follow the instructions in each comment block to
+complete the assignment.
+
+
+1. Days of the week
+2. Sum numbers
+3. Filter words by length
+4. Is letter in list?
+5. Is letter in word?
---
+++ lab3.rb
@@ -0,0 +1,63 @@
+##############################################################################
+# Make a method to display which day of the week it is.  This method should
+# have one input value, the number of the day to display. (1 should display Sunday,
+# 2 should display Monday, etc.)  Have the method display a message similar to:
+#    "Day number 3 is Tuesday."
+#
+# Use an array inside this method to hold the days of the week.
+# Remember that the first position of an array is 0 and the first day (not the
+# 0th day) of the week is Sunday.
+#
+# Remember that you must first define the method, then call the method.
+#
+# Setup a short script to :
+# - Ask for the number of the day of the week to show
+# - Pass that number into your method to display the message
+# - Repeat this until 0 is entered for the day to show
+# - If the person enters a number that is not between 1 and 7, show an error
+#   message and ask for them to enter a number again.
+#############################################################################
+
+
+
+##############################################################################
+# Write a method to sum (add up, or total) the numbers from an array.  The method
+# should take in an array of any length (including 0) as input and return back
+# the sum of all the numbers in the array.  If the array is empty, return 0.
+#
+# Test your method, then write the script below.
+#
+# Write a short script to:
+# - Ask the person for 5 numbers (Use a loop to do this, don't duplicate code.)
+# - Add the numbers into an array as they are entered
+# - (Don't forget to convert them to numbers!)
+# - Show the list of numbers that were entered
+# - Use the method you made to sum the numbers
+# - Display the total in a nice message
+#############################################################################
+
+
+
+
+##############################################################################
+# Make a method to find words of some length.  Have the method take 2 inputs:
+# a list of words and a length.  The method should return a new list
+# containing only words that have the desired length.  If no words have that
+# length, return an empty list.
+#############################################################################
+
+
+
+
+##############################################################################
+# Make a method to take a list of letters and a single letter. Return true
+# if the single letter is found in the list, and false otherwise.
+#############################################################################
+
+
+
+
+##############################################################################
+# Make a method to take a word and a single letter. Return true
+# if the single letter is found in the word, and false otherwise.
+#############################################################################
